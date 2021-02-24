a83bfd61b217fa78420b9d167ab7e195c2d7b2e3
Kulak Georgiy <kulakgeorgij@gmail.com>
2021-02-24 14:39:45 +0200

    Provide float operators for wrapper

--- src/exponantiation_operator.hpp
+++ src/exponantiation_operator.hpp
@@ -12,37 +12,42 @@ public:
     class InternalNumberWrapper
     {
     public:
-        InternalNumberWrapper ( long double number )
+        InternalNumberWrapper ( long double const number ) noexcept
             :   m_number{ number }
         {}
 
-        long double get () const
+        long double get () const noexcept
         {
             return m_number;
         }
 
     private:
-        long double m_number;
+        long double const m_number;
     };
 
-    NumberWrapper ( long double number )
+    NumberWrapper ( long double number ) noexcept
         :   m_number{ number }
     {}
 
-    InternalNumberWrapper operator* () const
+    InternalNumberWrapper operator* () const noexcept
     {
         return InternalNumberWrapper( m_number );
     }
 
-    //auto operator<=> ( NumberWrapper const & ) const noexcept = default;
+    auto operator<=> ( NumberWrapper const & ) const noexcept = default;
 
-    auto operator+ () const noexcept { return m_number; }
-    auto operator- () const noexcept { return -m_number; }
+    NumberWrapper operator+ () const noexcept { return m_number; }
+    NumberWrapper operator- () const noexcept { return -m_number; }
 
-    auto operator+ ( NumberWrapper const & n ) const noexcept { return n.m_number + m_number; }
-    auto operator- ( NumberWrapper const & n ) const noexcept { return n.m_number - m_number; }
-    auto operator* ( NumberWrapper const & n ) const noexcept { return n.m_number * m_number; }
-    auto operator/ ( NumberWrapper const & n ) const noexcept { return n.m_number / m_number; }
+    NumberWrapper operator+ ( NumberWrapper const & n ) const noexcept { return m_number + n.m_number; }
+    NumberWrapper operator- ( NumberWrapper const & n ) const noexcept { return m_number - n.m_number; }
+    NumberWrapper operator* ( NumberWrapper const & n ) const noexcept { return m_number * n.m_number; }
+    NumberWrapper operator/ ( NumberWrapper const & n ) const noexcept { return m_number / n.m_number; }
+
+    NumberWrapper & operator+= ( NumberWrapper const & n ) noexcept { m_number += n.m_number; return *this; }
+    NumberWrapper & operator-= ( NumberWrapper const & n ) noexcept { m_number -= n.m_number; return *this; }
+    NumberWrapper & operator*= ( NumberWrapper const & n ) noexcept { m_number *= n.m_number; return *this; }
+    NumberWrapper & operator/= ( NumberWrapper const & n ) noexcept { m_number /= n.m_number; return *this; }
 
     operator bool () const noexcept
     {
@@ -55,16 +60,16 @@ public:
     }
 
 private:
-    long double m_number;
+    long double m_number = 0;
 };
 
 // left ^ right == left ** right == left * (*right)
-auto operator* ( auto const n1, NumberWrapper::InternalNumberWrapper const n2 )
+NumberWrapper operator* ( NumberWrapper const & n1, NumberWrapper::InternalNumberWrapper const n2 )
 {
     return std::pow( n1, n2.get() );
 }
 
-NumberWrapper operator "" _n ( long double n )
+NumberWrapper operator "" _n ( long double n ) noexcept
 {
     return NumberWrapper( n );
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@ using namespace expon;
 int main ()
 {
     auto num1 = 2 ** 3.0_n;
-    assert( num1 == 8 );
+    assert( num1 == 8.0_n );
 
     NumberWrapper num2 = 0;
-    if ( !num2 )
-        std::cout << std::endl;//assert( num2 == 0.0 );
+    assert( num2 == 0.0_n );
+
+    NumberWrapper num3 = 4;
+    NumberWrapper num4 = 3;
+    assert( ( num3 ** num4 ) == 64.0_n );
+
+    num3 += 5;
+    assert( num3 == 9.0_n );
 
     return 0;
 }
